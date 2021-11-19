2ea1bdf93d1f1a0a077c7696657c9cca70d67460
Flávia Souza <sflavia051@gmail.com>
2021-11-18 23:11:25 -0300

    My hobby

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+document.getElementById("q1").innerHTML = "O que é JavaScript?";
+
+document.getElementById("r1").innerHTML = "JavaScript é uma linguagem de programação que permite a você implementar itens complexos em páginas web — toda vez que uma página da web faz mais do que simplesmente mostrar a você informação estática — mostrando conteúdo que se atualiza em um intervalo de tempo, mapas interativos ou gráficos 2D/3D animados, etc.";
+
+document.getElementById("q2").innerHTML = "Preenchendo Tags através do document.getElementById('id')?";
+
+document.getElementById("r2").innerHTML = "Pode-se utilizar o document.getElementById(id entre aspas).value = valor entre aspas";
---
+++ js/myhobby.js
@@ -0,0 +1,5 @@
+document.getElementById("title").innerHTML = "Meu maior hobby";
+
+document.getElementById("title2").innerHTML = "Conhecer novos lugares";
+
+document.getElementById("text").innerHTML = "Facilmente definido pelo termo Wanderlust, que traduzido do inglês significa desejo de viajar, meu principal hobby é algo que me acompanha desde a infância, mas que se tornou ainda mais notável quando, em 2017, eu conquistei uma oportunidade de intercâmbio e pude conhecer a província de NewfoundLand, no Canadá. Desde essa época, tenho tido como principal motivação para meus estudos a vontade de viajar pelo mundo e conhecer diferentes culturas e novos lugares.";
