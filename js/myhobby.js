f93f256820fa95e5ee9ee2a4dbdb08b70c1ad72b
Flávia Souza <sflavia051@gmail.com>
2021-11-18 23:30:40 -0300

    Slah

--- js/myhobby.js
+++ js/myhobby.js
@@ -1,5 +1,17 @@
+/*
+
+A página deve conter as seguintes informações:
+
+    a. Informações sobre você (Nome, idade, estado, foto);
+
+    b. Informações sobre o seu hobby (Título, descrição, links para consulta, imagens);
+
+*/
+
 document.getElementById("title").innerHTML = "Meu maior hobby";
 
 document.getElementById("title2").innerHTML = "Conhecer novos lugares";
 
 document.getElementById("text").innerHTML = "Facilmente definido pelo termo Wanderlust, que traduzido do inglês significa desejo de viajar, meu principal hobby é algo que me acompanha desde a infância, mas que se tornou ainda mais notável quando, em 2017, eu conquistei uma oportunidade de intercâmbio e pude conhecer a província de NewfoundLand, no Canadá. Desde essa época, tenho tido como principal motivação para meus estudos a vontade de viajar pelo mundo e conhecer diferentes culturas e novos lugares.";
+
+document.getElementById("aboutme").innerHTML = "Prazer, me chamo Flávia Souza, sou natural de Salgueiro (Pernambuco), tenho 19 anos e essa é a página na qual eu adiciono e implemento meu aprendizado na área de Web Development, então continue me acompanhando para ver minha evolução na área!";
