00257b126804d3ba7dd5aac1d6a7927e8e8df176
Flávia Souza <sflavia051@gmail.com>
2021-11-21 23:04:22 -0300

    Favorite Band

---
+++ js/iron-maiden.js
@@ -0,0 +1,12 @@
+document.querySelector("#title").innerHTML = "Meu favorite band";
+
+document.querySelector("#aboutme").innerHTML = "Prazer, me chamo Flávia Souza, sou natural de Salgueiro (Pernambuco), tenho 19 anos e essa é a página na qual eu adiciono e implemento meu aprendizado na área de Web Development, então continue me acompanhando para ver minha evolução na área!";
+
+document.querySelector("#title2").innerHTML = "Iron Maiden";
+
+document.querySelector("#estilo").innerHTML = "Heavy Metal";
+
+document.querySelector("#text").innerHTML = "Pioneira do movimento musical que ficou conhecido como NWOBHM (Nova Onda do Heavy Metal Britânico), Iron Maiden é uma banda britânica de heavy metal, formada em 1975 pelo baixista Steve Harris, cujo trabalho influenciou diversas bandas de rock e metal, das mais antigas às modernas, e é considerado um dos grupos mais importantes e influentes do estilo.";
+
+document.querySelector("#text2").innerHTML = "Minhas músicas preferidas são a Tears of the Dragon e a Dance of Death, você pode ouvi-las clicando nos links abaixo:";
+
