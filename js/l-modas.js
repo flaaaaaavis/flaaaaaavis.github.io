1af96d1aab930a802fa7aa18fc380309b09e2d0d
Flávia Souza <sflavia051@gmail.com>
2021-11-24 04:20:54 -0300

    Formulário de Avaliação

---
+++ js/l-modas.js
@@ -0,0 +1,35 @@
+var enviar = document.querySelector('#enviar');
+
+enviar.onclick = function(event) {
+
+    event.preventDefault();
+
+    var form = document.querySelector("#formulario");
+
+    var email = form.email.value;
+    var recomend = form.star1.value;
+    var atend = form.star2.value;
+    var comp = form.star3.value;
+
+    if(email != '' && recomend != '' && atend != '' && comp != '') {
+        var avaliacaoTr = document.createElement("tr");
+
+        document.getElementById("parent").appendChild(avaliacaoTr);
+
+        var emailTd = document.createElement("td");
+        var recomendTd = document.createElement("td");
+        var atendTd = document.createElement("td");
+        var compTd = document.createElement("td");
+
+        emailTd.textContent = email;
+        recomendTd.textContent = recomend;
+        atendTd.textContent = atend;
+        compTd.textContent = comp;
+
+        avaliacaoTr.appendChild(emailTd);
+        avaliacaoTr.appendChild(recomendTd);
+        avaliacaoTr.appendChild(atendTd);
+        avaliacaoTr.appendChild(compTd);
+    }
+
+};
---
+++ linux.md
@@ -0,0 +1,11 @@
+# Comandos
+
+## Apagar coisas
+
+### Apagar diretório
+    rmdir nome-do-diretório
+
+### Apagar arquivo
+    rm nome-do-arquivo
+
+OBS: O / antes do nome do diretório indica que é "dentro da raiz". Então, se você quer acessar um sub-diretório, não é correto usar /sub-diretório.
