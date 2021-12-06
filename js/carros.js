0f503db466c6ee9c710d56c45a2dd10c7e294dbd
Flávia Souza <sflavia051@gmail.com>
2021-12-05 22:41:38 -0300

    Carros 2
    Foram implementados os aprendizados iniciais sobre objetos ao projeto Carros

--- js/carros.js
+++ js/carros.js
@@ -1,7 +1,39 @@
-function calculateVelocity(minVelocity, maxVelocity, skid) {
-  let random = Math.floor(Math.random() * (maxVelocity - minVelocity)) + minVelocity;
-  result = random - random * skid/100;
-  return result
+let popular = {
+  maxv: Math.floor(Math.random() * (200 - 180)) + 180,
+  minv: Math.floor(Math.random() * (130 - 110)) + 110,
+  derrap: Math.floor(Math.random() * (4 - 3)) + 3,
+}
+let sport = {
+  maxv: Math.floor(Math.random() * (215 - 195)) + 195,
+  minv: Math.floor(Math.random() * (145 - 125)) + 125,
+  derrap: Math.floor(Math.random() * (3 - 3)) + 3,
+}
+let supersport = {
+  maxv: Math.floor(Math.random() * (230 - 210)) + 210,
+  minv: Math.floor(Math.random() * (160 - 140)) + 140,
+  derrap: Math.floor(Math.random() * (1.75 - 1)) + 1,
+}
+
+let car;
+
+function choosecar() {
+  let choosen = Math.floor(Math.random() * (20 - 1)) + 1;
+  
+  if(choosen > 0 && choosen <=12) {
+    car = popular;
+  } else if (choosen > 12 && choosen < 20) {
+    car = sport;
+  } else if (choosen == 20) {
+    car = supersport;
+  }
+}
+
+choosecar();
+
+function calculateVelocity() {
+  let random = Math.floor(Math.random() * (car.maxv - car.minv)) + car.minv;
+  result = random - random * car.derrap/100;
+  return result;
 }
 
 function greater(array) {
@@ -23,9 +55,9 @@ function race(laps) {
     let wins = [0, 0 ,0];
     let pedro, juca, edna;
     for (let i = 0; i < laps; i++) {
-        pedro = calculateVelocity(150, 230, 3);
-        juca  = calculateVelocity(120, 260, 5);
-        edna  = calculateVelocity(180, 220, 1);
+        pedro = calculateVelocity();
+        juca  = calculateVelocity();
+        edna  = calculateVelocity();
         wins[greater([pedro, juca, edna])] +=1;
     }
   return greater(wins);
