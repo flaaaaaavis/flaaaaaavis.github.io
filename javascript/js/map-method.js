89de25c28bb9ed690ad6371dc493cff1b3a0a2fc
Flávia Souza <sflavia051@gmail.com>
2021-12-16 01:36:33 -0300

    Map Method
    Calculo dos Juros corrigido

--- javascript/js/map-method.js
+++ javascript/js/map-method.js
@@ -18,9 +18,12 @@ function keepit() {
 }
 
 function pushToObject() {
+    let subarray = {};
     subarray["cliente"] = nomeCliente;
     subarray["vencimento"] = dataCompra;
     subarray["valor"] = parseFloat(valorCompra);
+    subarray["juros"] = "";
+    subarray["final"] = "";
 
     array.push(subarray);
 
@@ -28,62 +31,139 @@ function pushToObject() {
 }
 
 let jurosTd, finalTd;
+let contadorTr = -1;
 
 function sendToDocument() {
+    contadorTr++;
     let node = document.createElement("tr");
+    node.setAttribute("id", contadorTr);
     let tableB = document.getElementById("tabela");
     tableB.appendChild(node);
 
     let nomeTd = document.createElement("td");
     let nomeText = document.createTextNode(nomeCliente);
     nomeTd.appendChild(nomeText);
+    nomeTd.setAttribute("id", contadorTr+"-cliente");
     node.appendChild(nomeTd);
 
     let vencimentoTd = document.createElement("td");
     let vencimentoText = document.createTextNode(dataCompra);
     vencimentoTd.appendChild(vencimentoText);
+    vencimentoTd.setAttribute("id", contadorTr+"-vencimento");
     node.appendChild(vencimentoTd);
 
     let valorTd = document.createElement("td");
     let valorText = document.createTextNode(valorCompra);
     valorTd.appendChild(valorText);
+    valorTd.setAttribute("id", contadorTr+"-valor");
     node.appendChild(valorTd);
 
     jurosTd = document.createElement("td");
+    jurosTd.setAttribute("id", contadorTr+"-juros");
     node.appendChild(jurosTd);
 
     finalTd = document.createElement("td");
+    finalTd.setAttribute("id", contadorTr+"-final");
     node.appendChild(finalTd);
 }
 
 let calc = document.getElementById('calcular');
 calc.onclick = function (){
-    mapArray();
+    array.forEach((item, index) => {
+        let hoje = new Date();
+        let juros;
+        let valorFinal;
+        let valorJuros;
+    
+        let prazo = new Date(item.vencimento);
+        let Difference_In_Time = hoje.getTime() - prazo.getTime();
+        let Difference_In_Days = parseInt(Difference_In_Time / (1000 * 3600 * 24));
+    
+        if (Difference_In_Days > 0) {
+            juros = (Difference_In_Days*0.1 + 2).toFixed(2);
+            valorJuros = juros / 100;
+            valorFinal = (item.valor + item.valor*valorJuros).toFixed(2);
+        } else {
+            valorFinal = (item.valor).toFixed(2);
+        }
+
+        if(item["juros"] == ""){
+            item["juros"] = juros;
+            document.getElementById(index + "-juros").textContent = juros;
+        }
+
+        if(item["final"] == ""){
+            item["final"] = valorFinal;
+            document.getElementById(index + "-final").textContent = valorFinal;
+        }
+        
+    });
+    console.log(array);
 };
 
 
-function mapArray() {
-    let hoje = new Date();
-    let prazo, juros, valorFinal;
 
-    array.map(compra => {
-        prazo = new Date(compra.vencimento);
-        let Difference_In_Time = hoje.getTime() - prazo.getTime();
-        let Difference_In_Days = parseInt(Difference_In_Time / (1000 * 3600 * 24));
-        if (Difference_In_Days > 0) {
-            juros = Difference_In_Days*0.1 + 2;
-            let valorJuros = juros / 100;
-            valorFinal = (compra.valor + compra.valor*valorJuros).toFixed(2);
-        }
-    });
+// let porNome = document.getElementById('porNome');
+// porNome.onclick = function() {
+//     array.sort( compare );
+//     sendReorder();
+// }
 
-    subarray["juros"] = juros;
-    let jurosText = document.createTextNode(juros + "%");
-    jurosTd.appendChild(jurosText);
+// let vazio = "";
 
-    subarray["final"] = valorFinal;
-    let finalText = document.createTextNode(valorFinal);
-    finalTd.appendChild(finalText);
+// function substituir(item) {
+//     let node = document.createElement("tr");
+//     let tableB = document.getElementById("tabela");
+//     tableB.appendChild(node);
 
-    console.log(juros, valorFinal);
-}
+//     let nomeTd = document.createElement("td");
+//     let nomeText = document.createTextNode(item.cliente);
+//     nomeTd.appendChild(nomeText);
+//     node.appendChild(nomeTd);
+
+//     let vencimentoTd = document.createElement("td");
+//     let vencimentoText = document.createTextNode(item.vencimento);
+//     vencimentoTd.appendChild(vencimentoText);
+//     node.appendChild(vencimentoTd);
+
+//     let valorTd = document.createElement("td");
+//     let valorText = document.createTextNode(item.valor);
+//     valorTd.appendChild(valorText);
+//     node.appendChild(valorTd);
+
+//     jurosTd = document.createElement("td");
+//     node.appendChild(jurosTd);
+
+//     finalTd = document.createElement("td");
+//     node.appendChild(finalTd);
+// }
+
+// function sendReorder() {
+//     let tableB = document.getElementById("tabela");
+//     tableB.append(vazio);
+
+//     array.forEach(substituir);
+// }
+
+// function compare( a, b ) {
+//     if ( a.cliente < b.cliente ){
+//         return -1;
+//     }
+//     if ( a.cliente > b.cliente ){
+//         return 1;
+//     }
+//     return 0;
+//     sendReorder();
+// }
+
+// let porNome = document.getElementById('porNome');
+// porNome.onclick = array.sort((a, b) => {
+//     if(a.cliente > b.cliente) {
+//         return 1
+//     } else {
+//         return -1
+//     }
+// });
+
+// console.log(array);
+      
